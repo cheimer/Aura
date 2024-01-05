d96c1ceaf65e2d50ee410a417aca194bf5694104
cheimer <cheimer1337@gmail.com>
2024-01-05 16:59:41 +0900

    24.01.05 - Add Character

---
+++ Source/Aura/Public/Player/AuraPlayerController.h
@@ -0,0 +1,39 @@
+
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "AuraPlayerController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AURA_API AAuraPlayerController : public APlayerController
+{
+	GENERATED_BODY()
+
+public:
+	AAuraPlayerController();
+
+	virtual void PlayerTick(float DeltaTime) override;
+	
+protected:
+	virtual void BeginPlay() override;
+	virtual void SetupInputComponent() override;
+
+private:
+	void Move(const struct FInputActionValue& InputActionValue);
+
+	void CursorTrace();
+	
+	UPROPERTY(EditAnywhere, Category = "Input")
+	TObjectPtr<class UInputMappingContext> AuraContext;
+
+	UPROPERTY(EditAnywhere, Category = "Input")
+	TObjectPtr<class UInputAction> MoveAction;
+
+	class IEnemyInterface* LastActor;
+	class IEnemyInterface* ThisActor;
+};
